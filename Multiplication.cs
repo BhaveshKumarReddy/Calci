a6ff7fe9849f7c63be8c2634124f1cfa790a4131
Neha <nehadhruwa.9102@gmail.com>
2023-03-30 14:22:19 +0530

    Multiplication is created

---
+++ Multiplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calci
+{
+    internal class Multiplication
+    {
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
