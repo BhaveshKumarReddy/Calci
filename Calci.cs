4411754456fe24ceb759fb4b808f0f42cf1fb284
BhaveshKumarReddy <Admin@DESKTOP-HHR16QE>
2023-03-30 15:27:11 +0530

    created Calci Class

---
+++ Calci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calci
+{
+    class Calci
+    {
+        public int Addition(int a,int b) {
+            return Add.add(1,2);
+        }
+    }
+}
